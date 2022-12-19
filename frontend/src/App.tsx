15469e4f10e356cc33af4601d4f74feb0138c687
gacmen45 <marekgacek45@gmail.com>
2022-12-19 08:08:20 +0100

    add apollo client

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,17 +3,26 @@ import './App.module.scss'
 import Home from './pages/home/Home'
 import Navigation from './components/navigation/Navigation'
 import { Routes, Route,BrowserRouter } from 'react-router-dom'
+import { ApolloClient,InMemoryCache,ApolloProvider } from '@apollo/client'
+
+const client = new ApolloClient({
+   uri: 'http://localhost:1337/graphql',
+   cache: new InMemoryCache()
+})
+
 
 function App() {
 	return (
-		<div>
          <BrowserRouter>
+         <ApolloProvider client={client} >
+		<div>
 				<Navigation />
 			<Routes>
          <Route path='/' element={<Home/>}/>
 			</Routes>
-         </BrowserRouter>
 		</div>
+      </ApolloProvider>
+         </BrowserRouter>
 	)
 }
 
