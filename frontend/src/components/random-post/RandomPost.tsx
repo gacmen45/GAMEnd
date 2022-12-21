306a5cc98a7ce0c2b6cb41731858754afa048fe7
gacmen45 <marekgacek45@gmail.com>
2022-12-21 12:45:43 +0100

    finish random post

--- frontend/src/components/random-post/RandomPost.tsx
+++ frontend/src/components/random-post/RandomPost.tsx
@@ -1,34 +1,29 @@
-import { useAllPosts } from "../../hooks/useAllPosts"
-import PostCard from "../post-card/PostCard"
+import { useAllPosts } from '../../hooks/useAllPosts'
+import PostCard from '../post-card/PostCard'
+import styles from './RandomPost.module.scss'
 
 const RandomPost = () => {
-    const { loading, error, data } = useAllPosts()
+	const { loading, error, data } = useAllPosts()
 
-    if (loading) return <p>loading...</p>
+	if (loading) return <p>loading...</p>
 	if (error) return <p>error...</p>
 
-    // console.log('randomPost',data.posts.data[2]);
+	const randomNumber = Math.floor(Math.random() * data.posts.data.length)
+	const randomPost = data.posts.data[randomNumber]
 
-    const randomNumber =Math.floor( Math.random()*(data.posts.data.length))
-
-   const randomPost = data.posts.data[randomNumber]
-
-   console.log('randomPost' , randomPost);
-
-return <div>
-    <p>test</p>
-    <PostCard
-    key={randomPost.id}
-    id={randomPost.id}
-    title={randomPost.attributes.title}
-    content={randomPost.attributes.content}
-    publishedAt={randomPost.attributes.publishedAt}
-    image={randomPost.attributes.image.data.attributes.url}
-    categories={randomPost.attributes.categories.data.map((category:Post)=>category.attributes.name)}
-    />
-</div>
-    
-
+	return (
+		<div className={styles['random__container']}>
+			<PostCard className={styles['random__post']}
+				key={randomPost.id}
+				id={randomPost.id}
+				title={randomPost.attributes.title}
+				content={randomPost.attributes.content}
+				publishedAt={randomPost.attributes.publishedAt}
+				image={randomPost.attributes.image.data.attributes.url}
+				categories={randomPost.attributes.categories.data.map((category: Post) => category.attributes.name)}
+			/>
+		</div>
+	)
 }
 
 export default RandomPost
