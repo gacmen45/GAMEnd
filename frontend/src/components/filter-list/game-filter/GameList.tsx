54a56b63a530511a29f89a7c9c2b0ad24d672f78
gacmen45 <marekgacek45@gmail.com>
2022-12-21 20:49:57 +0100

    fix bug with img

--- frontend/src/components/filter-list/game-filter/GameList.tsx
+++ frontend/src/components/filter-list/game-filter/GameList.tsx
@@ -1,5 +1,5 @@
 import { useGames } from '../../../hooks/useGame'
-import FilterListItems from '../FilterListItems'
+import FilterListItems from '../FilterList'
 import Card from '../../UI/wrapper/Card/Card'
 import styles from './GameList.module.scss'
 import { Link } from 'react-router-dom'
@@ -11,21 +11,23 @@ const GameList = () => {
 	if (error) return <p>error...</p>
 
 	const allItems = data.games.data
+	const title = 'Gry'
 
 	return (
-		<Card className={styles['game-list__container']}>
-			<h2 className={styles['game-list__header']}>Lista Gier:</h2>
-			<ul className={styles['game-list__list']} >
-				{allItems.map(game => (
-					<Link to={`/game/${game.id}`} className={styles['game-list__link']}>
-						<FilterListItems
-							imgUrl={game.attributes.image.data.map(game => game.attributes.url)}
-							title={game.attributes.title}
-						/>
-					</Link>
-				))}
-			</ul>
-		</Card>
+		<FilterListItems title={title} allItems={allItems} />
+		// <Card className={styles['game-list__container']}>
+		// 	<h2 className={styles['game-list__header']}>Lista Gier:</h2>
+		// 	<ul className={styles['game-list__list']} >
+		// 		{allItems.map(game => (
+		// 			<Link to={`/game/${game.id}`} className={styles['game-list__link']}>
+		// 				<FilterListItems
+		// 					imgUrl={game.attributes.image.data.map(game => game.attributes.url)}
+		// 					title={game.attributes.title}
+		// 				/>
+		// 			</Link>
+		// 		))}
+		// 	</ul>
+		// </Card>
 	)
 }
 export default GameList
--- frontend/src/components/filter-list/tag-filter/TagFilter.tsx
+++ frontend/src/components/filter-list/tag-filter/TagFilter.tsx
@@ -2,7 +2,7 @@ import styles from './TagFilter.module.scss'
 import { useTags } from '../../../hooks/useTags'
 import Card from '../../UI/wrapper/Card/Card'
 import { Link } from 'react-router-dom'
-import FilterListItems from '../FilterListItems'
+import FilterListItems from '../FilterList'
 
 const TagFilter = () => {
 	const { loading, error, data } = useTags()
@@ -11,22 +11,24 @@ const TagFilter = () => {
 	if (error) return <p>error...</p>
 
 	const allItems = data.tags.data
+	const title = 'Sortuj według tagów:'
 
 	return (
-		<Card className={styles['tag-list__container']}>
-			<h2 className={styles['tag-list__header']}>Sortuj według tagów:</h2>
-			<ul className={styles['tag-list__list']}>
-				{allItems.map(tag => (
-					<Link to={`/tag/${tag.id}`} className={styles['tag-list__link']}>
-						<FilterListItems
-							imgUrl={tag.attributes.image.data.attributes.url}
-							title={tag.attributes.title}
-							amount={tag.attributes.posts.data.length}
-						/>
-					</Link>
-				))}
-			</ul>
-		</Card>
+		<FilterListItems title={title} allItems={allItems} />
+		// <Card className={styles['tag-list__container']}>
+		// 	<h2 className={styles['tag-list__header']}>Sortuj według tagów:</h2>
+		// 	<ul className={styles['tag-list__list']}>
+		// 		{allItems.map(tag => (
+		// 			<Link to={`/tag/${tag.id}`} className={styles['tag-list__link']}>
+		// 				<FilterListItems
+		// 					imgUrl={tag.attributes.image.data.attributes.url}
+		// 					title={tag.attributes.title}
+		// 					amount={tag.attributes.posts.data.length}
+		// 				/>
+		// 			</Link>
+		// 		))}
+		// 	</ul>
+		// </Card>
 	)
 }
 
