bc760e58008505015634b4796725fcc5141708a1
gacmen45 <marekgacek45@gmail.com>
2022-12-21 09:54:48 +0100

    create header component

--- frontend/src/components/footer/Footer.tsx
+++ frontend/src/components/footer/Footer.tsx
@@ -1,14 +1,23 @@
 import styles from './Footer.module.scss'
 import gacmen from '../../assets/logo.png'
-const Footer = () => {
-    return <footer>
-         <div className={styles["footer__author"]}>
-            <p className={styles["footer__author-text"]}>design by <a href="https://github.com/gacmen45">
-                <img
-                        src={gacmen} alt=""/>gacmen45</a></p>
-        </div>
-    </footer>
-}
+import Header from '../header/Header'
 
+const Footer = () => {
+	return (
+		<footer>
+			<Header />
+
+			<div className={styles['footer__author']}>
+				<p className={styles['footer__author-text']}>
+					design by{' '}
+					<a href='https://github.com/gacmen45'>
+						{/* <img src={gacmen} alt='' /> */}
+						Marek Gacek
+					</a>
+				</p>
+			</div>
+		</footer>
+	)
+}
 
 export default Footer
